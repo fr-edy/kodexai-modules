f86ff16297669d28c8325eabf8c851804edf5d42
Peet <gh@peet.ws>
2025-01-28 16:50:07 +0100

    chore: add ttl cache, delete proxies

--- assignment_2/utils.py
+++ assignment_2/utils.py
@@ -1,5 +1,7 @@
 import logging
 import requests
+import functools
+import time
 
 log = logging.getLogger(__name__)
 
@@ -39,3 +41,26 @@ def load_page_content(url: str, params: dict = None) -> str:
         f"Loaded link '{url}' with status {resp.status_code} and content length {len(resp.text)}"
     )
     return resp.text
+
+
+def time_cache(max_age, maxsize=128, typed=False):
+    """Least-recently-used cache decorator with time-based cache invalidation.
+
+    Args:
+        max_age: Time to live for cached results (in seconds).
+        maxsize: Maximum cache size (see `functools.lru_cache`).
+        typed: Cache on distinct input types (see `functools.lru_cache`).
+    """
+
+    def _decorator(fn):
+        @functools.lru_cache(maxsize=maxsize, typed=typed)
+        def _new(*args, __time_salt, **kwargs):
+            return fn(*args, **kwargs)
+
+        @functools.wraps(fn)
+        def _wrapped(*args, **kwargs):
+            return _new(*args, **kwargs, __time_salt=int(time.time() / max_age))
+
+        return _wrapped
+
+    return _decorator
